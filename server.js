d9e9c6aafc075ae9e05541af32876f503607ebd2
atika67 <30780041+atika67@users.noreply.github.com>
2017-08-14 18:02:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,73 +4,56 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-var articles={
-'article-one':{
-title:'ARTICLE-ONE ATIKA',
-heading:'ARTICLE ONE',
-date:'SEP 5, 2016',
-content:`<p> This is the first article: content for my webapp is here: content for my webapp is here: content for my webapp is here: content for my webapp is here: content for my webapp is here: content for my webapp is here: content for my webapp is here: content for my webapp is here:
-</p>
-<p>
-content for my webapp is here:content for my webapp is here:content for my webapp is here:content for my webapp is here:content for my webapp is here:content for my webapp is here:content for my webapp is here:content for my webapp is h:content for my webapp is here:
-</p>
-<p>
-content for my webapp is here:content for my webapp is here:content for my webapp is here:content for my webapp is here:content for my webapp is here:content for my webapp is here:content for my webapp is here:content for my webapp is h:content for my webapp is here:
-</p>`
-
-},
-'article-two':{
-title:'ARTICLE-TWO ATIKA',
-heading:'ARTICLE TWO',
-date:'SEP 10, 2016',
-content:`
-<p> content for my second article </p>`
-},
-'article-three':{
-title:'ARTICLE-THREE ATIKA',
-heading:'ARTICLE THREE',
-date:'SEP 15, 2016',
-content:`
-<p> content for my third article </p>`  
-}
+var articleOne={
+        title:'ARTICLE-ONE ATIKA',
+        heading:'ARTICLE ONE',
+        date:'SEP 5, 2016',
+        content:`<p> This is the first article: content for my webapp is here: content for my webapp is here: content for my webapp is here: content for my webapp is here: content for my webapp is here: content for my webapp is here: content for my webapp is here: content for my webapp is here:
+        </p>
+        <p>
+        content for my webapp is here:content for my webapp is here:content for my webapp is here:content for my webapp is here:content for my webapp is here:content for my webapp is here:content for my webapp is here:content for my webapp is h:content for my webapp is here:
+        </p>
+        <p>
+        content for my webapp is here:content for my webapp is here:content for my webapp is here:content for my webapp is here:content for my webapp is here:content for my webapp is here:content for my webapp is here:content for my webapp is h:content for my webapp is here:
+        </p>`
 };
 function createTemplate (data){
     var title=data.title;
     var heading=data.heading;
     var date=data.date;
     var content=data.content;
-var htmlTemplate=`
-<html>
-<head>
-   <link href="/ui/style.css" rel="stylesheet" />
-    <title> ${title} </title>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-</head>
-<body>
-    <div class="container">
-    <div>
-        <a href="/"> Home </a>
-    </div>
-    <hr/>
-    <h3> ${heading} </h3>
-    <div>
-        ${date}
-    </div>
-    <div>
-        ${content}
-    </div>
-    </div>
-</body>
-</html>`;
+    var htmlTemplate=`
+    <html>
+    <head>
+       <link href="/ui/style.css" rel="stylesheet" />
+        <title> ${title} </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/"> Home </a>
+        </div>
+        <hr/>
+        <h3> ${heading} </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}
+        </div>
+        </div>
+    </body>
+    </html>`;
 return htmlTemplate;
 }
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/:articleName', function (req, res) {
-   res.send(createTemplate(articles[articleName]));
-   var articleName=req.params.articleName;
+app.get('/article-one', function (req, res) {
+   res.send(createTemplate(articleOne));
+   //var articleName=req.params.articleName;
    // articleName==article-one
    // article[articleName]=={} content object for articleone
 });
