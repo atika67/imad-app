536a7392c617c7e4d4ede65b018e268c09dee56e
atika67 <30780041+atika67@users.noreply.github.com>
2017-08-14 17:14:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/:articleName', function (req, res) {
-   res.send(createTemplate(articles[articleName]));
+   res.send(createTemplate(articles[article-one]));
    var articleName=req.params.articleName;
    // articleName==article-one
    // article[articleName]=={} content object for articleone
