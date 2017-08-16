96432943530511d4d26316e31aaa40b1416f6a66
atika67 <30780041+atika67@users.noreply.github.com>
2017-08-16 23:16:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var element =document.getElementById('main-text');
 element.innerHTML='New Value';
 // move the image
-var img=docoment.getElementById('madi');
+var img=document.getElementById('madi');
 img.onclick=function(){
     img.style.marginLeft='100px';
 }
