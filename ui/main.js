863a1b749887b56c20ae2b11958e4c4a56cf564c
atika67 <30780041+atika67@users.noreply.github.com>
2017-08-20 20:20:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,40 +1,19 @@
-//console.log('Loaded!');
-// change the text of the main text-div
-//var element =document.getElementById('main-text');
-//element.innerHTML='New Value';
-// move the image
-// var img=document.getElementById('madi');
-//var marginLeft=0;
-//function moveRight(){
-    //marginLeft=marginLeft+1;
-    //img.style.marginLeft=marginLeft+'px'; 
-//}
-//img.onclick=function(){
-   // var interval=setInterval(moveRight,50);
-   // img.style.marginLeft='100px';
-//};
-// counter code
 var button=document.getElementById('counter');
 button.onclick=function(){
-var request=new XMLHttpRequest();
-request.onreadystatechange=function(){
-    if(request.readystate=== XMLHttpRequest.DONE){
-        // take some action
-if(request.atatus===200){
-    var counter=request.responseText;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
-}
-}
-//not done yet
-};
-//var counter=0;
-
-//button.onclick=function(){
-    // make a request to the counter end point 
-    //capture the response and store it in the variable and render the variable in correct span
-   // counter=counter+1;
-   request.open('GET','http://atika1498.imad.hasura-app.io/counter',true);
-   request.send(null);
-    
-};
+    // create a request object
+    var request=new XMLHttpRequest();
+    //capture the response and store it in a variable
+        request.onreadystatechange=function(){
+                if(request.readystate=== XMLHttpRequest.DONE){
+                    // take some action
+                        if(request.status===200){
+                        var counter=request.responseText;
+                        var span=document.getElementById('count');
+                        span.innerHTML=counter.toString();
+                        }
+            }
+            //not done yet
+        };
+       request.open('GET','http://atika1498.imad.hasura-app.io/counter',true);
+       request.send(null);
+    };
