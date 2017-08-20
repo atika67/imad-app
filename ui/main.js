927430085ce3188afd04172cc5af886c01357044
atika67 <30780041+atika67@users.noreply.github.com>
2017-08-20 20:39:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,23 @@
 var button=document.getElementById('counter');
-var counter=0;
 button.onclick=function(){
     // create a request object
-    counter=counter+1;
-    
+    button.onclick=function(){
+    // create a request object
+    var request=new XMLHttpRequest();
+    //capture the response and store it in a variable
+        request.onreadystatechange=function(){
+                if(request.readystate=== XMLHttpRequest.DONE){
+                    // take some action
+                        if(request.status===200){
+                        var counter=request.responseText;
                         var span=document.getElementById('count');
                         span.innerHTML=counter.toString();
-                        };
+                        }
+            }
+            //not done yet
+        };
+    };
+       request.open('GET','http://atika1498.imad.hasura-app.io/counter',true);
+       request.send(null);
+ };
           
