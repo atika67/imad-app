40fd6ef9b1a0b20d131b1838f518433fc8c41e76
atika67 <30780041+atika67@users.noreply.github.com>
2017-09-05 21:50:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,8 +19,8 @@ var button=document.getElementById('counter');
  };
 //submit name
 var nameInput=document.getElementById('name');
-//var name=nameInput.value;
-var submit=document.getElementById('submit_btn');
+var name=nameInput.value;
+var submit=document.getElementById('sbtn');
 submit.onclick=function(){
   var names=['name1','name2','name3','name4'];
   var list='';
